50ba4346accd7e7a4fe147f3b012a52db913e5d4
Theodore Bendixson <tedbendixson@me.com>
2016-07-17 16:41:28 -0400

    Initial Commit

---
+++ iOSUnitTestsIntro/Pokemon.swift
@@ -0,0 +1,51 @@
+//
+//  Pokemon.swift
+//  iOSUnitTestsIntro
+//
+//  Created by Theodore Bendixson on 7/17/16.
+//  Copyright © 2016 X-Team. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon{
+    
+    var type:PokemonType
+    var attackType:PokemonAttackType
+    var health:Int = 100
+    
+    init(type:PokemonType, attackType:PokemonAttackType){
+        self.type = type
+        self.attackType = attackType
+    }
+    
+    func attack(enemy:Pokemon){
+    
+        var damage = 30
+        
+        if enemy.type == .Fire && attackType == .Water{
+            damage = 60
+        }
+        
+        if enemy.type == .Water && attackType == .Fire{
+            damage = 10
+        }
+        
+        enemy.health = enemy.health - damage
+    
+    }
+    
+    
+}
+
+enum PokemonType{
+    case Neutral
+    case Fire
+    case Water
+}
+
+enum PokemonAttackType{
+    case Normal
+    case Fire
+    case Water
+}
---
+++ iOSUnitTestsIntroTests/PokemonTests.swift
@@ -0,0 +1,56 @@
+//
+//  PokemonTests.swift
+//  iOSUnitTestsIntro
+//
+//  Created by Theodore Bendixson on 7/17/16.
+//  Copyright © 2016 X-Team. All rights reserved.
+//
+
+import XCTest
+@testable import iOSUnitTestsIntro
+
+class PokemonTests: XCTestCase {
+
+    var squirtle:Pokemon!
+    var charmander:Pokemon!
+    var psyduck:Pokemon!
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        squirtle = Pokemon(type: .Water, attackType: .Water)
+        charmander = Pokemon(type: .Fire, attackType: .Fire)
+        psyduck = Pokemon(type: .Water, attackType: .Water)
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        squirtle = nil
+        charmander = nil
+        psyduck = nil
+    }
+
+    func testThatAWaterPokemonDoesMoreDamageToAFirePokemon(){
+        
+        //1. Have Squirtle Attack Charmander
+        squirtle.attack(charmander)
+        
+        //Ouch!
+        
+        //2. Now Have Squirtle Attack Psyduck
+        squirtle.attack(psyduck)
+        
+        //Oooh. That had to hurt.
+        
+        //Who took more damage? Charmander or Psyduck?
+        
+        //If a Water Pokemon Does More Damage To A Fire Pokemon,
+        //We would expect Charmander to have less health than Psyduck.
+        
+        //3. Test it
+        XCTAssertTrue(charmander.health < psyduck.health)
+        
+    }
+    
+}
